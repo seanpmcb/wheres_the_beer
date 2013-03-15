67f52b347a213065a7048f883e05ff4fe9694344
Sean P. McBride <sean@wapolabs.com>
2013-03-15 11:19:26 -0400

    create app

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+requires = [
+    'pyramid',
+    'pyramid_debugtoolbar',
+    'waitress',
+    ]
+
+setup(name='wheres_the_beer',
+      version='0.0',
+      description='wheres_the_beer',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="wheres_the_beer",
+      entry_points="""\
+      [paste.app_factory]
+      main = wheres_the_beer:main
+      """,
+      )
